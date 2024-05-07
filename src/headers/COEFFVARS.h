c572b8748dec024bc7f698561261f0a6b9f4a0c6
SahajSJain <67571850+SahajSJain@users.noreply.github.com>
2024-05-07 16:36:54 -0400

    Delete src directory

--- src/headers/COEFFVARS.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "./CLASSES.h"
-#include <eigen3/Eigen/Dense>
-namespace COEFFVARS{
-    inline CLASSES::SolverCoeffs ADC; // Coeffs for Advections diffusion equation
-    inline CLASSES::SolverCoeffs DEq; // Coeffs for
-    inline Eigen::VectorXd GCOx(1), GCOy(1); //inverse of dx(i)+dx(i+1); used for gradient calculations
-    inline Eigen::VectorXd CoE(1), CoW(1), CoN(1), CoS(1); //Coefficients for Half node calculations
-}
--- src/headers/FLOWVARS.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "./CLASSES.h"
-namespace FLOWVARS{
-    inline CLASSES::PrimFlowField New, Old, Older;
-    inline CLASSES::FractionalStepArrays FSA;
-}
